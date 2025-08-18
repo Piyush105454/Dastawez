dbc335c379834c18c30705d9a5b417fa3cab17f5
Piyush105454 <piyushmodi812@gmail.com>
2025-08-19 01:07:03 +0530

    fix: resolve build errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@supabase/realtime-js"],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
